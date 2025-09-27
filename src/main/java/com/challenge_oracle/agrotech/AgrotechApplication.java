e908a4dc413029b3078cdbdfd71711e6d247198d
ftessmann <fernando.tessmann@gmail.com>
2025-09-27 16:11:02 -0300

    feat: start project

---
+++ src/main/java/com/challenge_oracle/agrotech/AgrotechApplication.java
@@ -0,0 +1,13 @@
+package com.challenge_oracle.agrotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge_oracle/agrotech/AgrotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge_oracle.agrotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
