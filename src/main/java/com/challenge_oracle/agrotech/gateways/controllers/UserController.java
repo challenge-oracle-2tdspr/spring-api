d8d5f96405b936ca605a19c5f9f992115f4f9c32
ftessmann <fernando.tessmann@gmail.com>
2025-10-06 19:58:05 -0300

    feat: add swagger documentation on user endpoint

--- src/main/java/com/challenge_oracle/agrotech/gateways/controllers/UserController.java
+++ src/main/java/com/challenge_oracle/agrotech/gateways/controllers/UserController.java
@@ -4,6 +4,11 @@ import com.challenge_oracle.agrotech.gateways.requests.UserCreateRequestDTO;
 import com.challenge_oracle.agrotech.gateways.requests.UserUpdateRequestDTO;
 import com.challenge_oracle.agrotech.gateways.responses.UserResponseDTO;
 import com.challenge_oracle.agrotech.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,14 +20,24 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+import static java.util.Optional.empty;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Tag(name = "Users", description = "User managing endpoints")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping
+    @Operation(summary = "Create new user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input date", content = @Content),
+            @ApiResponse(responseCode = "409", description = "Email or CPF already exists", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     public ResponseEntity<UserResponseDTO> createUser (
             @Valid
             @RequestBody
@@ -33,6 +48,12 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Fetch all users with pagination")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Users fetched"),
+            @ApiResponse(responseCode = "400", description = "Invalid pagination parameters", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     public ResponseEntity<Page<UserResponseDTO>> getAllUsers(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size
@@ -43,12 +64,27 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Fetch one user by its id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User fetched"),
+            @ApiResponse(responseCode = "400", description = "Invalid UUID format", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
+    })
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID id) {
         UserResponseDTO user = userService.getUserById(id);
         return ResponseEntity.ok(user);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update user")
+    @ApiResponses({
+            @ApiResponse(responseCode ="200",description = "User updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data or validation error", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content),
+            @ApiResponse(responseCode = "409", description = "Email or CPF already exists", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+})
     public ResponseEntity<UserResponseDTO> updateUser(
             @PathVariable UUID id,
             @RequestBody UserUpdateRequestDTO dto
@@ -58,6 +94,13 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "User deleted"),
+            @ApiResponse(responseCode = "400", description = "Invalid UUID format", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     public ResponseEntity<UserResponseDTO> deleteUser(@PathVariable UUID id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
